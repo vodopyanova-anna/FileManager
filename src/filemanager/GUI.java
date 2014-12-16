dd3fdbb2f782d8e44971dd30c7e1998bccbf8fe2
VKgit <kote.godue@gmail.com>
2014-12-16 02:47:26 +0200

    disk name + disk space = only for inintial state

--- src/filemanager/GUI.java
+++ src/filemanager/GUI.java
@@ -1,10 +1,13 @@
 package filemanager;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.io.File;
+import java.util.*;
 
 public class GUI{
-	private JComboBox<String> gcb, gcb2;
+	private JComboBox gcb, gcb2;
 	private JButton helpButton, createFolderButton, copyButton, renameButton, deleteButton, terminalButton, exitButton;
 	private JTable table, table2;
 	private JScrollPane scroll, scroll2;
@@ -15,7 +18,82 @@ public class GUI{
 	private int frameSizeX = 800;
 	private int frameSizeY = 600;
 	private int countButton = 7;
-	public void makeGUI(){ 		
+
+
+      // This inner class is needed to input icons + text in comboBoxes
+        public class IconListRenderer
+                extends DefaultListCellRenderer {
+
+                private Map<Object, Icon> icons = null;
+
+                public IconListRenderer(Map<Object, Icon> icons) {
+                        this.icons = icons;
+                }
+
+                @Override
+                public Component getListCellRendererComponent(
+                        JList list, Object value, int index,
+                        boolean isSelected, boolean cellHasFocus) {
+
+                        // Get the renderer component from parent class
+
+                        JLabel label =
+                                (JLabel) super.getListCellRendererComponent(list,
+                                        value, index, isSelected, cellHasFocus);
+
+                        // Get icon to use for the list item value
+
+                        Icon icon = icons.get(value);
+
+                        // Set icon to display for value
+
+                        label.setIcon(icon);
+                        return label;
+                }
+        }
+        /* Inner Class ends here*/
+
+        // This method fills combobox with disk lettesr and icons
+        public void fillingDisksCombos(JComboBox diskComboBox) {
+                Map<Object, Icon> icons = new HashMap<Object, Icon>();
+
+                java.util.List<File> files = Arrays.asList(File.listRoots());
+
+                for (File f : files) {
+                        icons.put(f.toString(), FileSystemView.getFileSystemView().getSystemIcon(f));
+                }
+
+                for (File f : files) {
+                        diskComboBox.addItem(f.toString());
+                }
+
+                diskComboBox.setRenderer(new IconListRenderer(icons)); // this string is for correct output of combobox
+        }
+        //This is only for initiaization
+        public void fillingDisksNameLabels(JLabel diskLabel){
+                File f[]=File.listRoots();
+                String [] parts = FileSystemView.getFileSystemView().getSystemDisplayName(f[0]).toString().split(" ");
+                diskLabel.setText("["+parts[0]+"]");
+        }
+        //This is only for initiaization
+        public void fillingDisksSpaceLabels(JLabel diskLabel){
+                File f[]=File.listRoots();
+                long Usable = f[0].getUsableSpace()/1024; // in kilobytes
+                long Total = f[0].getTotalSpace()/1024; // in kilobytes
+                diskLabel.setText(Usable+" kilobytes from "+Total+" kilobytes");
+        }
+
+        //Вывод имен дисков типов дисков может пригодится
+       /*
+       for (File f : files) {
+           //System.out.println(FileSystemView.getFileSystemView().getSystemDisplayName(f));
+          // System.out.println(FileSystemView.getFileSystemView().getSystemTypeDescription(f));
+          // System.out.println(f);
+          getTotalSpace()
+          getFreeSpace()
+          }*/
+
+        public void makeGUI(){
         JFrame frame = new JFrame("File Manager");    
         gbl = new GridBagLayout();
         gbc = new GridBagConstraints();
@@ -29,37 +107,43 @@ public class GUI{
         gbc.anchor = GridBagConstraints.WEST;        
         gbc.gridx = 0;
         gbc.gridy = 0;  
-        String disks [] = {"c", "d", "flash"};
-        gcb = new JComboBox<String> (disks);         
+
+        gcb = new JComboBox();
+        fillingDisksCombos(gcb);
         frame.add(gcb, gbc); 
         
         gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.ipadx = 5;
-        labelDisk = new JLabel ("[work]");
+        labelDisk = new JLabel();
+                fillingDisksNameLabels(labelDisk);
         frame.add(labelDisk, gbc);  
         
         gbc.gridx = 2;
         gbc.gridy = 0;        
-        labelMemory = new JLabel ("100 000 000 �� �������� �� 250 000 000 ��");
+        labelMemory = new JLabel();
+                fillingDisksSpaceLabels(labelMemory);
         frame.add(labelMemory, gbc);  
         
         gbc.gridx = 3;
         gbc.gridy = 0; 
         gbc.ipadx = 0;
-        gcb2 = new JComboBox<String> (disks);
-        frame.add(gcb2, gbc); 
+        gcb2 = new JComboBox();
+        fillingDisksCombos(gcb2); // here are we filling first combo box with valid info
+        frame.add(gcb2, gbc);
         
         gbc.gridx = 4;
         gbc.gridy = 0; 
         gbc.ipadx = 5;
-        labelDisk2 = new JLabel ("[reserv]");
-        frame.add(labelDisk2, gbc);  
+        labelDisk2 = new JLabel();
+        fillingDisksNameLabels(labelDisk2);
+        frame.add(labelDisk2, gbc);
         
         gbc.gridx = 5;
         gbc.gridy = 0;        
-        labelMemory2 = new JLabel ("125 545 000 �� �������� �� 454 545 000 ��");
-        frame.add(labelMemory2, gbc); 
+        labelMemory2 = new JLabel ();
+                fillingDisksSpaceLabels(labelMemory2);
+        frame.add(labelMemory2, gbc); // here are we filling second combo box with valid info
         
         gbc.gridx = 0;
         gbc.gridy = 1;        
@@ -131,15 +215,14 @@ public class GUI{
     
 	
     public static void main(String[] args) {
-            //test push
-      /*	SwingUtilities.invokeLater(new Runnable(){
+
+     	SwingUtilities.invokeLater(new Runnable(){
        		public void run(){
        			GUI fmGUI = new GUI();    
        			fmGUI.makeGUI();
        		}
-       	});*/
-            GUI2 GUI_2 = new GUI2();
-            GUI_2.Test();
+       	});
+
    	}
 
 }
