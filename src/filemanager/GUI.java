14783a67448708c5942a43f182c1ba3bdb658879
VKgit <kote.godue@gmail.com>
2014-12-18 02:20:17 +0200

    Jlist+initializing it with all files and folder hidden + system = feature not a bug

--- src/filemanager/GUI.java
+++ src/filemanager/GUI.java
@@ -14,7 +14,8 @@ public class GUI{
 	private JLabel labelDisk, labelDisk2, labelMemory, labelMemory2, labelCommandLine;
 	private JTextField commandLine;
 	private GridBagLayout gbl;
-	private GridBagConstraints gbc;	
+	private GridBagConstraints gbc;
+    private JList tempchoice;
 	private int frameSizeX = 800;
 	private int frameSizeY = 600;
 	private int countButton = 7;
@@ -83,6 +84,22 @@ public class GUI{
                 diskLabel.setText(Usable+" kilobytes from "+Total+" kilobytes");
         }
 
+        public void fillingList(JList window,  DefaultListModel listModel) {
+
+            Map<Object, Icon> icons = new HashMap<Object, Icon>();
+            File f[] = File.listRoots();
+
+            java.util.List<File> files = Arrays.asList(f[0].listFiles());// all files eve hidden
+
+            for (File iterator : files) {
+              icons.put(FileSystemView.getFileSystemView().getSystemDisplayName(iterator), FileSystemView.getFileSystemView().getSystemIcon(iterator));
+            }
+
+            for(File iterator : files) {
+                listModel.addElement(FileSystemView.getFileSystemView().getSystemDisplayName(iterator));
+            }
+           window.setCellRenderer(new IconListRenderer(icons));
+                  }
         //useful info to deletion
        /*
        for (File f : files) {
@@ -92,131 +109,131 @@ public class GUI{
           }*/
 
         public void makeGUI(){
-        JFrame frame = new JFrame("File Manager");    
+        JFrame frame = new JFrame("File Manager");
         gbl = new GridBagLayout();
         gbc = new GridBagConstraints();
         frame.setLayout(gbl);
         frame.setSize(frameSizeX, frameSizeY);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         gbc.weightx = 0.5;
-        gbc.weighty = 0.5; 
-        
-        gbc.anchor = GridBagConstraints.WEST;        
+        gbc.weighty = 0.5;
+
+        gbc.anchor = GridBagConstraints.WEST;
         gbc.gridx = 0;
-        gbc.gridy = 0;  
+        gbc.gridy = 0;
 
         gcb = new JComboBox();
         fillingDisksCombos(gcb);
-        frame.add(gcb, gbc); 
-        
+        frame.add(gcb, gbc);
+
         gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.ipadx = 5;
         labelDisk = new JLabel();
                 fillingDisksNameLabels(labelDisk);
-        frame.add(labelDisk, gbc);  
-        
+        frame.add(labelDisk, gbc);
+
         gbc.gridx = 2;
-        gbc.gridy = 0;        
+        gbc.gridy = 0;
         labelMemory = new JLabel();
                 fillingDisksSpaceLabels(labelMemory);
-        frame.add(labelMemory, gbc);  
-        
+        frame.add(labelMemory, gbc);
+
         gbc.gridx = 3;
-        gbc.gridy = 0; 
+        gbc.gridy = 0;
         gbc.ipadx = 0;
         gcb2 = new JComboBox();
         fillingDisksCombos(gcb2); // here are we filling first combo box with valid info
         frame.add(gcb2, gbc);
-        
+
         gbc.gridx = 4;
-        gbc.gridy = 0; 
+        gbc.gridy = 0;
         gbc.ipadx = 5;
         labelDisk2 = new JLabel();
         fillingDisksNameLabels(labelDisk2);
         frame.add(labelDisk2, gbc);
-        
+
         gbc.gridx = 5;
-        gbc.gridy = 0;        
+        gbc.gridy = 0;
         labelMemory2 = new JLabel ();
+
                 fillingDisksSpaceLabels(labelMemory2);
         frame.add(labelMemory2, gbc); // here are we filling second combo box with valid info
-        
+
         gbc.gridx = 0;
-        gbc.gridy = 1;        
+        gbc.gridy = 1;
         gbc.ipadx = 380;
-        gbc.ipady = 460; 
+        gbc.ipady = 460;
         gbc.gridwidth = 3;
         String[] colHeads = {"Name", "Extension", "Size"};
-        Object [][] data = {
-        		{"My folder", "", "1234"},
-        		{"My folder", "", "12345"},
-        		{"My file", "txt", "146"},
-        		{"My file2", "doc", "1234"},
-        		{"My photo", "jpeg", "454545"},
-        };
-        table = new JTable(data, colHeads);
-        scroll = new JScrollPane(table);        
-        frame.add(scroll, gbc); 
-		
+
+
+
+            DefaultListModel listModel = new DefaultListModel();
+        tempchoice = new JList(listModel);
+
+        fillingList(tempchoice,listModel);
+        scroll = new JScrollPane(tempchoice);
+        frame.add(scroll, gbc);
+
         gbc.gridx = 3;
         gbc.gridy = 1;
         Object [][] data2 = {
         		{"folder", "", "1234"},
         		{"folder", "", "12345"},
-        		{"file", "txt", "146"},        		
+        		{"file", "txt", "146"},
         };
         table2 = new JTable(data2, colHeads);
-        scroll2 = new JScrollPane(table2);       
-        frame.add(scroll2, gbc);  
-                    
-        gbc.ipady = 15; 
+        scroll2 = new JScrollPane(table2); //Please Explain
+        frame.add(scroll2, gbc);
+
+        gbc.ipady = 15;
         gbc.gridx = 0;
-        gbc.gridy = 2; 
+        gbc.gridy = 2;
         labelCommandLine = new JLabel("C:\\>");
         frame.add(labelCommandLine, gbc);
-        
+
         commandLine = new JTextField();
         gbc.gridx = 1;
-        gbc.gridy = 2; 
-        gbc.gridwidth = 8;  
+        gbc.gridy = 2;
+        gbc.gridwidth = 8;
         gbc.ipadx = 724;
         commandLine = new JTextField();
         frame.add(commandLine, gbc);
-        
+
         helpButton = new JButton("F1-Help");
-        createFolderButton = new JButton("F2-New folder");        
+        createFolderButton = new JButton("F2-New folder");
         copyButton = new JButton("F5-Copy");
-        renameButton = new JButton("F6-Rename");        
-        deleteButton = new JButton("F8-Delete");        
-        terminalButton = new JButton("F9-Terminal");        
+        renameButton = new JButton("F6-Rename");
+        deleteButton = new JButton("F8-Delete");
+        terminalButton = new JButton("F9-Terminal");
         exitButton = new JButton("F10-Exit");
-        
-        JPanel buttonPanel = new JPanel();  
+
+        JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1, countButton));
-        buttonPanel.add(helpButton);        
-        buttonPanel.add(createFolderButton);        
-        buttonPanel.add(copyButton);        
+        buttonPanel.add(helpButton);
+        buttonPanel.add(createFolderButton);
+        buttonPanel.add(copyButton);
         buttonPanel.add(renameButton);
         buttonPanel.add(deleteButton);
         buttonPanel.add(terminalButton);
         buttonPanel.add(exitButton);
         gbc.gridx = 0;
-        gbc.gridy = 3;        
-        gbc.gridwidth = GridBagConstraints.REMAINDER;        
-        frame.add(buttonPanel, gbc); 
-        
+        gbc.gridy = 3;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        frame.add(buttonPanel, gbc);
+
         frame.setResizable(false);
-        frame.setVisible(true);        
-	} 
-    
-	
+        frame.setVisible(true);
+	}
+
+
     public static void main(String[] args) {
 
      	SwingUtilities.invokeLater(new Runnable(){
        		public void run(){
-       			GUI fmGUI = new GUI();    
+       			GUI fmGUI = new GUI();
        			fmGUI.makeGUI();
        		}
        	});
@@ -224,4 +241,5 @@ public class GUI{
    	}
 
 }
+
 
